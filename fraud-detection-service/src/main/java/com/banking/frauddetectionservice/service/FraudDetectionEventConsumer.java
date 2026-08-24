f8d0964e5668e93fe2fdaed8201d7fc43e82782a
Siddharth3271 <formaledusiddh371@gmail.com>
2026-08-24 23:27:48 +0530

    Fraud detection service started

---
+++ fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionEventConsumer.java
@@ -0,0 +1,30 @@
+package com.banking.frauddetectionservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FraudDetectionEventConsumer {
+
+    private final FraudDetectionService fraudDetectionService;
+
+    //Listens to transaction.initiated topic and every transaction goes through fraud check before completing
+    @KafkaListener(topics = "transaction.initiated",groupId = "fraud-detection-group")
+    public void consumeTransactionInitiated(@Payload Map<String,Object>payload){
+        log.info("Received Transaction for fraud check: {}",payload.get("transactionId"));
+
+        try{
+            fraudDetectionService.checkTransaction(payload);
+        }
+        catch(Exception e){
+
+        }
+    }
+}
